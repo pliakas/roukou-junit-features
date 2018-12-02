21db33a37e56baf460e0801d6760e310791d5e82
pliakas <thomas.pliakas@cloudstreet.co>
2018-12-02 14:18:35 +0200

    Initial project for demostrating junit5 features

--- src/main/java/org/roukou/junit/features/RoukouJunitFeaturesApplication.java
+++ src/main/java/org/roukou/junit/features/RoukouJunitFeaturesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RoukouJunitFeaturesApplication
 {
 
+
     public static void main( String[] args )
     {
         SpringApplication.run( RoukouJunitFeaturesApplication.class, args );
